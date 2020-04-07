e96e347f3e437a6e9e04f64b12d69809b460a0f7
Adarsh Singh <2001adarshsingh@gmail.com>
2020-04-07 22:37:19 +0530

    updated

--- app/src/main/java/com/adarsh/farmket/login.java
+++ app/src/main/java/com/adarsh/farmket/login.java
@@ -2,13 +2,43 @@ package com.adarsh.farmket;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
-public class login extends AppCompatActivity {
-
+public class login extends AppCompatActivity implements View.OnClickListener{
+    private Button login;
+    private TextView goRegister;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        setui();
+        login.setOnClickListener(this);
+        goRegister.setOnClickListener(this);
+
+    }
+    private void setui(){
+        login = (Button) findViewById(R.id.login);
+        goRegister = (TextView) findViewById(R.id.goregister);
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()){
+            case R.id.login:
+                Intent it = new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(it);
+                finish();
+                break;
+            case R.id.goregister:
+                Intent ip = new Intent(getApplicationContext(), register.class);
+                startActivity(ip);
+                finish();
+                break;
+        }
     }
 }
--- app/src/main/java/com/adarsh/farmket/register.java
+++ app/src/main/java/com/adarsh/farmket/register.java
@@ -2,13 +2,42 @@ package com.adarsh.farmket;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
-public class register extends AppCompatActivity {
+public class register extends AppCompatActivity implements View.OnClickListener{
+    private Button register;
+    private TextView goLogin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+        setui();
+
+        register.setOnClickListener(this);
+        goLogin.setOnClickListener(this);
+
+    }
+
+    private void setui(){
+        register = (Button) findViewById(R.id.register);
+        goLogin = (TextView) findViewById(R.id.goLogin);
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()){
+            case R.id.register:
+                break;
+            case R.id.goLogin:
+                Intent it = new Intent(getApplicationContext(), login.class);
+                startActivity(it);
+                finish();
+                break;
+        }
     }
 }
