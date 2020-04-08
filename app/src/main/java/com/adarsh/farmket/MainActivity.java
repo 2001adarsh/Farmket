fd9fac0cbb728c95a75d7b3d181a7b29de2c2ec9
Adarsh Singh <2001adarshsingh@gmail.com>
2020-04-08 23:23:28 +0530

    updated

--- app/src/main/java/com/adarsh/farmket/MainActivity.java
+++ app/src/main/java/com/adarsh/farmket/MainActivity.java
@@ -1,14 +1,55 @@
 package com.adarsh.farmket;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.FrameLayout;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationView;
+
+public class MainActivity extends AppCompatActivity implements BottomNavigationView.OnNavigationItemSelectedListener {
+
+    private BottomNavigationView bottomNav;
+    private FrameLayout frameLayout;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        setui();
+
+        bottomNav.setOnNavigationItemSelectedListener(this);
+
+    }
+    private void setui(){
+
+        frameLayout = (FrameLayout) findViewById(R.id.main_frame);
+        bottomNav = (BottomNavigationView) findViewById(R.id.main_nav);
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()){
+
+            case R.id.nav_home:
+                Toast.makeText(this, "Home Fragment!", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.nav_cart:
+                Toast.makeText(this, "Cart Fragment", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.nav_help:
+                Toast.makeText(this, "Help", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.nav_profile:
+                Toast.makeText(this, "Profile", Toast.LENGTH_SHORT).show();
+                break;
+        }
+        return true;
     }
 }
