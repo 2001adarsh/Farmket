2e6633ea2140140da29020226c04901710348f39
Adarsh Singh <2001adarshsingh@gmail.com>
2020-04-11 23:07:07 +0530

    Verified AppBar problem

--- app/src/main/java/com/adarsh/farmket/ui/HomeFragment.java
+++ app/src/main/java/com/adarsh/farmket/ui/HomeFragment.java
@@ -2,6 +2,11 @@ package com.adarsh.farmket.ui;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.appcompat.widget.ToolbarWidgetWrapper;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
@@ -13,15 +18,30 @@ import com.adarsh.farmket.R;
 
 public class HomeFragment extends Fragment {
 
+    Toolbar toolbar;
+
     public HomeFragment() {
         // Required empty public constructor
     }
-
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false);
+
+        View view =inflater.inflate(R.layout.fragment_home, container, false);
+
+        Toolbar toolbar = view.findViewById(R.id.toolbar);
+        ((AppCompatActivity) getActivity()).setSupportActionBar(toolbar);
+
+        return view;
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+
+
     }
 }
