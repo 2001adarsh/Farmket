c98f797b4ea0776237fc7e5d3b976d4df31ca571
Adarsh Singh <2001adarshsingh@gmail.com>
2020-04-19 22:17:45 +0530

    Profile page edited

--- app/src/main/java/com/adarsh/farmket/ui/ProfileFragment.java
+++ app/src/main/java/com/adarsh/farmket/ui/ProfileFragment.java
@@ -2,28 +2,35 @@ package com.adarsh.farmket.ui;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.adarsh.farmket.R;
 
-/**
- * A simple {@link Fragment} subclass.
- */
 public class ProfileFragment extends Fragment {
 
     public ProfileFragment() {
         // Required empty public constructor
     }
 
-
+    private Toolbar toolbar;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_profile, container, false);
+        View view = inflater.inflate(R.layout.fragment_profile, container, false);
+        toolbar = view.findViewById(R.id.toolbar_profile);
+        ((AppCompatActivity) getActivity()).setSupportActionBar(toolbar);
+        return view;
     }
+
+
 }
--- app/src/main/java/com/adarsh/farmket/ui/UpdateFragment.java
+++ app/src/main/java/com/adarsh/farmket/ui/UpdateFragment.java
@@ -1,19 +1,18 @@
 package com.adarsh.farmket.ui;
 
+
 import android.os.Bundle;
-
-import androidx.fragment.app.Fragment;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import androidx.fragment.app.Fragment;
+
 import com.adarsh.farmket.R;
 
-/**
- * A simple {@link Fragment} subclass.
- */
 public class UpdateFragment extends Fragment {
+
+
 
     public UpdateFragment() {
         // Required empty public constructor
