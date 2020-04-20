8cd6ee94eaba5153bb4cefc857ae178f9080e522
Adarsh Singh <2001adarshsingh@gmail.com>
2020-04-20 15:10:48 +0530

    Toolbar fixed

--- app/src/main/java/com/adarsh/farmket/ui/ProfileFragment.java
+++ app/src/main/java/com/adarsh/farmket/ui/ProfileFragment.java
@@ -10,8 +10,10 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import com.adarsh.farmket.R;
 
@@ -29,10 +31,28 @@ public class ProfileFragment extends Fragment {
         View view = inflater.inflate(R.layout.fragment_profile, container, false);
         toolbar = view.findViewById(R.id.toolbar_profile);
         ((AppCompatActivity) getActivity()).setSupportActionBar(toolbar);
-        toolbar.inflateMenu(R.menu.profile_menu);
+        setHasOptionsMenu(true);
 
         return view;
     }
 
+    @Override
+    public void onCreateOptionsMenu(@NonNull Menu menu, @NonNull MenuInflater inflater) {
+            getActivity().getMenuInflater().inflate(R.menu.profile_menu, menu);
+            return;
+    }
 
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        int id = item.getItemId();
+
+        if(id == R.id.profile_help){
+            Toast.makeText(getActivity(), "Help", Toast.LENGTH_SHORT).show();
+        }
+        else if(id == R.id.about_us){
+            Toast.makeText(getActivity(), "About us", Toast.LENGTH_SHORT).show();
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
 }
